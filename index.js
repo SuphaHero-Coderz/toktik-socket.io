e66d7539d5d85939bff5c1c9555b980de996d988
Suphawit <suphawit.won@gmail.com>
2023-11-15 00:36:27 +0700

    added subscriber for new updates in video backend

--- index.js
+++ index.js
@@ -17,49 +17,24 @@ const io = require("socket.io")(httpServer, { cookie: true,
   const subscriber = client.duplicate();
   await subscriber.connect();
   subscriber.on('error', err => console.log('Redis Client Error', err));
-  await subscriber.subscribe('backend_updates', (message) => {
+  await subscriber.subscribe('backend_comments', (message) => {
     console.log(message); // 'message'
+    io.emit("new comments", message)
+  });
+  await subscriber.subscribe('backend_videos', (message) => {
+    console.log(message); // 'message'
+    io.emit("new updates", message)
   });
 })();
 
 const axios = require("axios");
 
 io.on("connection", (socket) => {
-	const cookies = socket.request.headers.cookie
-	console.log(cookies)
-	function getCookie(cName) {
-	   const name = cName + "=";
-	   const cDecoded = decodeURIComponent(cookies);
-	   const cArr = cDecoded.split(';');
-	   let res;
-	   cArr.forEach(val => {
-		  if (val.indexOf(name) === 0) res = val.substring(name.length);
-		  })
-	   return res;
-	}
-	const access_token = getCookie("access_token")
-  socket.on("view_video", (video_id) => {
-	console.log(socket.handshake.headers.cookie)
-	setInterval(async function() {
-    try {
-				console.log(video_id)
-				axios.get(`http://backend:80/api/get_views/${video_id}`,
-					{
-						withCredentials: true,
-						headers: {Cookie: `access_token=${access_token};`}
-					}).then((response) => {
-					io.emit("updated_views", response.data)
-					console.log(response);
-				});
-			} catch (error) {
-				console.log(error);
-			}
-	}, 5000);
-	});
+    console.log(socket.id)
 });
 
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 
 httpServer.listen(PORT, () =>
   console.log(`server listening at http://localhost:${PORT}`)
