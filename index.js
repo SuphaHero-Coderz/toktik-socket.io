585ee3cc06de256f77194e0ab729cb762e4ab779
mythicalbadger <jussy12.work@gmail.com>
2023-11-16 13:41:56 +0700

    subscription functionality for vip room stuff

--- index.js
+++ index.js
@@ -32,8 +32,18 @@ const io = require("socket.io")(httpServer, {
 	await subscriber.subscribe('new_notification', (message) => {
 		console.log(message);
 		const parsed = JSON.parse(message);
-		var video_owner_id = parsed[0].video_owner_id;
+		const video_owner_id = parsed[0].video_owner_id;
+		var subscribers = parsed[0].subscribers;
+		subscribers = subscribers ? subscribers : [];
+
+		console.log("There has been a new notification");
+
 		io.to(video_owner_id).emit("new_notification", JSON.stringify(parsed.slice(1)));
+
+		for (var i = 0; i < subscribers.length; i++) {
+			console.log(`Notifying user ${subscribers[i]}`);
+			io.to(subscribers[i]).emit("new_notification", JSON.stringify(parsed.slice(1)))
+		}
 	})
 })();
 
@@ -43,20 +53,22 @@ const axios = require("axios");
 // Middleware that checks username and allows the connection
 io.use((socket, next) => {
 	const user_id = socket.handshake.auth.user_id;
+	const subscriptions = socket.handshake.auth.subscriptions;
+
+	console.log(socket.handshake.auth);
+
 	if (!user_id) {
 		return next(new Error("Invalid user_id"));
 	}
 	socket.user_id = user_id;
+	socket.subscriptions = subscriptions ? subscriptions : [];
 	next();
 });
 
 
 io.on("connection", (socket) => {
 	socket.join(socket.user_id);
-
-	socket.on("join_vip", () => {
-		
-	});
+	console.log(`Joining user room: ${socket.user_id}`)
 });
 
 
